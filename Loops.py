f5528fbf5622120fdcbfe60be04c62e01b47ba9d
Isha-jainn <54052852+Isha-jainn@users.noreply.github.com>
2019-08-21 12:15:33 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,40 @@
+#Program for representing 'for' loop :
+
+#Program 1
+#To print numbers from 1 to 10
+
+i=1
+for i in range(1,11):
+    print(i)
+print("\n")
+
+#Prpgram 2
+#To print sum of first 10 natural numbers
+i=1
+s=0
+for i in range(1,11):
+    s=s+i
+print("The sum of first 10 natural numbers is" , s)
+print("\n\n")
+
+
+#Program for representing 'while' loop :
+
+#Program 1
+#To print numbers from 1 to 10
+
+i=1
+while i<11:
+    print(i)
+    i=i+1
+print("\n")
+
+
+#Program 2
+#To print sum of first 10 natural numbers
+i=1
+s=0
+while i<11:
+    s=s+i
+    i=i+1
+print("The sum of first 10 natural numbers is " , s)
