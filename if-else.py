d0d271d820c016f1405c667fb718a2d3265dfce2
Isha-jainn <54052852+Isha-jainn@users.noreply.github.com>
2019-08-21 11:49:54 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,38 @@
+#program for representing if:
+
+
+num=5
+if(num==5):
+    print("Number is 5")
+
+
+
+#program for representing if else:
+
+
+a=5
+b=int(input("\n\nEnter any number="))
+s=a+b
+if(s>=10):
+    print("Your value is greater than 5")
+else:
+    print("Your value is less than 5")
+
+
+#program for representing elif:
+
+
+num1=int(input("\n\nEnter a number - "))
+num2=int(input("Enter second number - "))
+num3=int(input("Enter third number - "))
+if(num1>num2 and num1>num3):
+    print("first number is greater")
+elif(num2>num1 and num2>num3):
+    print("second number is greater")
+elif(num3>num1 and num3>num2):
+    print("Third number is greater")
+else:
+    print("All numbers are equal")
+
+
+                            
